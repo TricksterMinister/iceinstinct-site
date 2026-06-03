d9016d815665ffe39b08eae8eddb7e52aed91797
TricksterMinister <temo.benidze@gmail.com>
2026-06-02 23:30:14 -0400

    feat(react): funnel store + Duality stylesheet

--- app/src/main-home.tsx
+++ app/src/main-home.tsx
@@ -1,6 +1,7 @@
 import './styles/accent.css';
 import './styles/cinema.css';
 import './styles/footer.css';
+import './styles/duality.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Home } from './Home';
